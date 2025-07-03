a8541ecccc497a868cdaec6c4c9240a14744088a
andyhorridge <andyhorridge@gmail.com>
2025-07-03 22:30:00 +0100

    Add files via upload

---
+++ familytree-config.js
@@ -0,0 +1,56 @@
+const chart_config = {
+  "chart": {
+    "container": "#tree-simple",
+    "rootOrientation": "NORTH",
+    "connectors": {
+      "type": "step"
+    },
+    "node": {
+      "HTMLclass": "node-name"
+    }
+  },
+  "nodeStructure": {
+    "text": {
+      "name": "John Embling"
+    },
+    "children": [
+      {
+        "text": {
+          "name": "Gerard Embling"
+        },
+        "children": [
+          {
+            "text": {
+              "name": "Sarah Louise Embling"
+            },
+            "children": []
+          },
+          {
+            "text": {
+              "name": "Luke Ashley Embling"
+            },
+            "children": []
+          },
+          {
+            "text": {
+              "name": "Eve Helena Embling"
+            },
+            "children": []
+          }
+        ]
+      },
+      {
+        "text": {
+          "name": "Malcom Embling"
+        },
+        "children": []
+      },
+      {
+        "text": {
+          "name": "John Embling"
+        },
+        "children": []
+      }
+    ]
+  }
+}
